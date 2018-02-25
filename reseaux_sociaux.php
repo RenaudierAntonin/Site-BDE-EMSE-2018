671fafb79a755cc4ab31ea659e0d606504ceec05
arthurlagel <arthur.lagel@laposte.net>
2018-02-26 00:34:59 +0100

    ajout de la page facebook et ouverture des réseaux sociaux dans des nouveaux onglets

--- reseaux_sociaux.php
+++ reseaux_sociaux.php
@@ -1,8 +1,8 @@
 <hr>
 	<div class="social-container">
-		<a href=""><img class="social" src="img/facebook_logo.svg"></a>
-		<a href=""><a href=""><img class="social" src="img/snapchat_logo.svg"></a>
-		<a href=""><img class = "social" src="img/instagram_logo.svg"></a>
-		<a href=""><img class="social" src="img/youtube_logo.svg"></a>
+		<a href="https://www.facebook.com/minesperium/" target="_blank"><img class="social" src="img/facebook_logo.svg"></a>
+		<a href="" target="_blank"><img class="social" src="img/snapchat_logo.svg"></a>
+		<a href="" target="_blank"><img class = "social" src="img/instagram_logo.svg"></a>
+		<a href="" target="_blank"><img class="social" src="img/youtube_logo.svg"></a>
 	</div>
 <hr>
