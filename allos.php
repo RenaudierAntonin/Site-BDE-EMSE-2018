270ccb862c2efe3420b8338d6dec9ad44556fa35
arthurlagel <arthur.lagel@laposte.net>
2018-03-10 12:18:12 +0100

    Numéros et conditions des allos

--- allos.php
+++ allos.php
@@ -11,21 +11,35 @@
 	<?php include("menu.php"); ?>
 
 	<div class="grid-container">
-		<a href="" class="grid-element"><div class="allo allo_film"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_car"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_laleuleu"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_capotes"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_binouze"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_clopes"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_victime"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_liaison"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_lecture"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_compagnie"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_dodo"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_blackout"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_reveil"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_coiffure"></div></a>
-		<a href="" class="grid-element"><div class="allo allo_massage"></div></a>
+		<a href="pages_allos/allo_film.html" class="grid-element"><div class="allo allo_film"></div></a>
+		
+		<a href="pages_allos/allo_car.html" class="grid-element"><div class="allo allo_car"></div></a>
+		
+		<a href="pages_allos/allo_laleuleu.html" class="grid-element"><div class="allo allo_laleuleu"></div></a>
+		
+		<a href="pages_allos/allo_capotes.html" class="grid-element"><div class="allo allo_capotes"></div></a>
+		
+		<a href="pages_allos/allo_binouze.html" class="grid-element"><div class="allo allo_binouze"></div></a>
+		
+		<a href="pages_allos/allo_clopes.html" class="grid-element"><div class="allo allo_clopes"></div></a>
+		
+		<a href="pages_allos/allo_victime.html" class="grid-element"><div class="allo allo_victime"></div></a>
+		
+		<a href="pages_allos/allo_liaison.html" class="grid-element"><div class="allo allo_liaison"></div></a>
+		
+		<a href="pages_allos/allo_lecture.html" class="grid-element"><div class="allo allo_lecture"></div></a>
+		
+		<a href="pages_allos/allo_compagnie.html" class="grid-element"><div class="allo allo_compagnie"></div></a>
+		
+		<a href="pages_allos/allo_dodo.html" class="grid-element"><div class="allo allo_dodo"></div></a>
+		
+		<a href="pages_allos/allo_blackout.html" class="grid-element"><div class="allo allo_blackout"></div></a>
+		
+		<a href="pages_allos/allo_reveil.html" class="grid-element"><div class="allo allo_reveil"></div></a>
+		
+		<a href="pages_allos/allo_coiffure.html" class="grid-element"><div class="allo allo_coiffure"></div></a>
+		
+		<a href="pages_allos/allo_massage.html" class="grid-element"><div class="allo allo_massage"></div></a>
 
 	</div>
 	<?php include("reseaux_sociaux.php"); ?>
