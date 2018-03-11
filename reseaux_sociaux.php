637e9661cb0620845a50aaa76f14e7e00edaf7ed
arthurlagel <arthur.lagel@laposte.net>
2018-03-11 19:13:28 +0100

    snapchat

--- reseaux_sociaux.php
+++ reseaux_sociaux.php
@@ -1,7 +1,7 @@
 <hr>
 	<div class="social-container">
 		<a href="https://www.facebook.com/minesperium/" target="_blank"><img class="social" src="img/facebook_logo.svg"></a>
-		<a href="" target="_blank"><img class="social" src="img/snapchat_logo.svg"></a>
+		<a href="snapchat.html" target="_blank"><img class="social" src="img/snapchat_logo.svg"></a>
 		<a href="" target="_blank"><img class="social" src="img/youtube_logo.svg"></a>
 	</div>
 <hr>
