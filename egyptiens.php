9014ffbf413a8b184d75f89e78b9e75b5f06ae7b
Matthias Ramos <matthias.ramos@etu.emse.fr>
2018-03-11 16:33:31 +0100

    ajout membres

---
+++ egyptiens.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mines'perium</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/style_membres.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+	
+	<?php include("menu.php"); ?>
+	<div class="presentations">
+		<img class="respo" src="img/membres/0com1.jpg">
+
+	<?php for($i = 2; $i < 6 ; $i++){
+
+		echo "<img src='img/membres/0com".$i.".jpg'>";
+	}
+		echo "<img class='respo' src='img/membres/0sponsor1.jpg'>";
+
+	for($i = 2; $i < 5 ; $i++){
+
+		echo "<img src='img/membres/0sponsor".$i.".jpg'>";
+
+			}?>
+	</div>
+
+	
+	<?php include("reseaux_sociaux.php"); ?>
+
+</body>
+<footer>
+	<p class="credits">Liste BDE Mines Saint-Etienne 2K18 - Mines'perium - Copyright BDE's Nerds</p>
+</footer>
+</html>
---
+++ gaulois.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mines'perium</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/style_membres.css">
+	<meta charset="utf-8">
+</head>
+
+<body>
+	
+	<?php include("menu.php"); ?>
+	<div class="presentations">
+		<img class="respo" src="img/membres/0valm1.jpg">
+		<img src="img/membres/0valm2.png">
+
+	<?php for($i = 3; $i < 8; $i++){
+
+		echo "<img src='img/membres/0valm".$i.".jpg'>";
+	}?>
+	</div>
+
+	
+	<?php include("reseaux_sociaux.php"); ?>
+
+</body>
+<footer>
+	<p class="credits">Liste BDE Mines Saint-Etienne 2K18 - Mines'perium - Copyright BDE's Nerds</p>
+</footer>
+</html>
