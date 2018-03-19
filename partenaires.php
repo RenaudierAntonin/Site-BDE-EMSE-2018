fdfa07ace4227524d8cb98693becbcb8a0293dc1
arthurlagel <arthur.lagel@laposte.net>
2018-03-19 15:30:37 +0100

    test CSS réseaux sociaux

--- partenaires.php
+++ partenaires.php
@@ -1,5 +1,4 @@
-	<div class="social-container">
-		<p style="font-size: 10px;">Partenaire</p>
-		<a href="https://www.geekstore.fr/" target="_blank"><img class="social" src="img/geekstore.png"></a>
-	</div>
-<hr>
+<div class="social-container">
+	<p style="font-size: 10px;">Partenaire</p>
+	<a href="https://www.geekstore.fr/" target="_blank"><img class="social" src="img/geekstore.png"></a>
+</div>
