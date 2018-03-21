a767be8f5b12e951a4ff4b9873f328586156e4ae
arthurlagel <arthur.lagel@laposte.net>
2018-03-21 15:32:59 +0100

    nouveau partenaire bigmat

--- partenaires.php
+++ partenaires.php
@@ -1,4 +1,5 @@
 <div class="social-container">
 	<p style="font-size: 10px;">Partenaire</p>
 	<a href="https://www.geekstore.fr/" target="_blank"><img class="social" src="img/geekstore.png"></a>
+	<a href="" target="_blank"><img src="img/Logo_Bigmat_Sagra_quadri.jpg" class="social"></a>
 </div>
